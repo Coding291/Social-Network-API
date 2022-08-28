1904968c22fd23afb7587d5147de9cd81f320431
Muhammad Humayun <humayun@MacBook-Pro.home>
2022-08-28 02:18:24 -0400

    developed models

---
+++ models/Reaction.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const ReactionSchema = new Schema ({
+
+
+})
+
+const Reaction = model('user', ReactionSchema);
+
+module.exports = Reaction;
+
---
+++ models/Thought.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema ({
+
+
+})
+
+const Thought = model('user', ThoughtSchema);
+
+module.exports = Thought;
